d5fbead4ccf330ed4160c6f98e10b502776eebee
devlopper <kycdev@gmail.com>
2017-02-07 14:56:11 +0000

    Change-Id: I683334d790e967282ddf6b0af1de89aecca8a0e6

--- company-model/src/main/java/org/cyk/system/company/model/structure/CompanyReport.java
+++ company-model/src/main/java/org/cyk/system/company/model/structure/CompanyReport.java
@@ -1,5 +1,6 @@
 package org.cyk.system.company.model.structure;
 
+import java.io.InputStream;
 import java.io.Serializable;
 
 import lombok.Getter;
@@ -14,11 +15,20 @@ public class CompanyReport extends PartyReport implements Serializable {
 
 	private Boolean valueAddedTaxCollected = Boolean.FALSE;
 	
+	private InputStream backgroundImage;
+	private Boolean generateBackground=Boolean.FALSE;
+	private Boolean draftBackground=Boolean.FALSE;
+	
 	@Override
 	public void generate() {
 		super.generate();
 		if(Boolean.TRUE.equals(globalIdentifier.getGenerateImage()))
 			globalIdentifier.setImage(inputStream(provider.companyLogo().getBytes()));
+		if(Boolean.TRUE.equals(generateBackground))
+			if(Boolean.TRUE.equals(draftBackground))
+				backgroundImage = inputStream(provider.documentDraftBackground().getBytes());
+			else
+				backgroundImage = inputStream(provider.companyLogo().getBytes());
 		globalIdentifier.setName(provider.companyName());
 	}
 	
