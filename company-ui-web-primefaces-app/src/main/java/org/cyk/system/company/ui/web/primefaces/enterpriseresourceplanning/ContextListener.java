d489059cbd33ad5238ab5292737e0100acaf1ac5
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2018-07-15 15:55:16 +0000

    Change-Id: I819959a47a051c3c179575a416aaf03829d6f7c6

--- company-ui-web-primefaces-app/src/main/java/org/cyk/system/company/ui/web/primefaces/enterpriseresourceplanning/ContextListener.java
+++ company-ui-web-primefaces-app/src/main/java/org/cyk/system/company/ui/web/primefaces/enterpriseresourceplanning/ContextListener.java
@@ -2,64 +2,20 @@ package org.cyk.system.company.ui.web.primefaces.enterpriseresourceplanning;
 
 import java.io.Serializable;
 
-import javax.servlet.ServletContextEvent;
-
 import org.cyk.system.company.ui.web.primefaces.ServletContextListener;
-import org.cyk.ui.web.primefaces.mathematics.movement.MovementCollectionInventoryEditFormMasterPrepareListener;
-import org.cyk.utility.common.helper.ClassHelper;
 import org.cyk.utility.common.security.Shiro;
 
 @javax.servlet.annotation.WebListener
 public class ContextListener extends ServletContextListener implements Serializable {
 	private static final long serialVersionUID = -9042005596731665575L;
 	
-	@Override
-	public void __contextInitialized__(ServletContextEvent event) {
-		super.__contextInitialized__(event);
-		//ClassHelper.getInstance().map(MovementCollectionInventoryEditFormMasterPrepareListener.class, MovementCollectionInventoryEditFormMasterPrepareAdapter.class);
+	protected void __addFoldersForUser__(Shiro.Ini ini){
+		ini.addFoldersForUser("private1");
 	}
-	/*
+	
 	@Override
 	protected Class<?> __getMenuBuilderClass__() {
 		return MenuBuilder.class;
-	}*/
-	
-	/*@Override
-	public void __contextInitialized__(ServletContextEvent event) {
-		super.__contextInitialized__(event);
-		CompanyWebManager.getInstance().getListeners().add(new PrimefacesManager());
-		CompanyBusinessLayer.getInstance().enableEnterpriseResourcePlanning();
-		
-		AbstractCompanyReportProducer.Listener.COLLECTION.add(new AbstractCompanyReportProducer.Listener.Adapter.Default(){
-			private static final long serialVersionUID = 1L;
-			
-			@Override
-			public void processLabelValueCollection(AbstractIdentifiableReport<?> identifiable,LabelValueCollectionReport labelValueCollection) {
-				super.processLabelValueCollection(identifiable,labelValueCollection);
-				if(SaleCashRegisterMovementCollectionReportTemplateFile.LABEL_VALUES_PAYMENT.equals(labelValueCollection.getIdentifier())){
-					SaleCashRegisterMovementCollection saleCashRegisterMovementCollection = (SaleCashRegisterMovementCollection) identifiable.getSource();
-					labelValueCollection.getCollection().clear();
-					labelValueCollection.addLabelValues(new String[][]{
-						{"Date",Constant.EMPTY_STRING}
-						,{identifiable.getCreationDate(),Constant.EMPTY_STRING}
-						,{"Receipt No.",Constant.EMPTY_STRING}
-						,{identifiable.getCode(),Constant.EMPTY_STRING}
-						,{"Cashier Name",Constant.EMPTY_STRING}
-						,{ saleCashRegisterMovementCollection.getGlobalIdentifier().getOwner() instanceof Application ? saleCashRegisterMovementCollection.getGlobalIdentifier().getOwner().getName() 
-								: ((Person)saleCashRegisterMovementCollection.getGlobalIdentifier().getOwner()).getNames(),Constant.EMPTY_STRING}
-						,{"Parent",Constant.EMPTY_STRING}
-						//saleCashRegisterMovementCollection.getSale().getCustomer().getPerson().getNames()
-						,{"GET IT FROM CUSTOMER",Constant.EMPTY_STRING}	
-						,{"Received from",Constant.EMPTY_STRING}
-						,{saleCashRegisterMovementCollection.getCashRegisterMovement().getMovement().getSenderOrReceiverPersonAsString(),Constant.EMPTY_STRING}	
-					});
-				}
-			}
-		});
-	}*/
-	
-	protected void __addFoldersForUser__(Shiro.Ini ini){
-		ini.addFoldersForUser("private1");
 	}
 	
 }
--- company-ui-web-primefaces-app/src/main/java/org/cyk/system/company/ui/web/primefaces/enterpriseresourceplanning/MenuBuilder.java
+++ company-ui-web-primefaces-app/src/main/java/org/cyk/system/company/ui/web/primefaces/enterpriseresourceplanning/MenuBuilder.java
@@ -9,9 +9,12 @@ import org.cyk.system.root.model.mathematics.movement.MovementCollectionValuesTr
 import org.cyk.system.root.model.mathematics.movement.MovementCollectionValuesTransferAcknowledgement;
 import org.cyk.system.root.model.mathematics.movement.MovementGroup;
 import org.cyk.system.root.model.party.Store;
+import org.cyk.system.root.model.time.IdentifiablePeriod;
+import org.cyk.system.root.model.time.IdentifiablePeriodCollection;
+import org.cyk.system.root.model.time.IdentifiablePeriodType;
 import org.cyk.utility.common.userinterface.command.Menu;
 
-public class MenuBuilder extends org.cyk.ui.web.primefaces.resources.MenuBuilder implements Serializable  {
+public class MenuBuilder extends org.cyk.ui.web.primefaces.MenuBuilder implements Serializable  {
 	private static final long serialVersionUID = 1L;
 	
 	@Override
@@ -32,6 +35,10 @@ public class MenuBuilder extends org.cyk.ui.web.primefaces.resources.MenuBuilder
 		
 		menu.addNode("sale")
 			.addNodeActionListMany(Sale.class)
+		;
+		
+		menu.addNode("period")
+			.addNodeActionListMany(IdentifiablePeriod.class,IdentifiablePeriodType.class,IdentifiablePeriodCollection.class)
 		;
 	}
 	
